e9c41c0b3d6323aac3d35413acfe0a83bc570852
RajaniVerma277 <rajaniverma507@gmail.com>
2025-07-16 18:49:04 +0530

    add file

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -32,10 +35,11 @@ app.get("/", (req, res)=>{
 });
 
 //index route
-app.get("/listings",async (req, res)=>{
+app.get("/listings", wrapAsync(async (req, res)=>{
     const allListings = await Listing.find({});
-    res.render("listings/index",{allListings});
-});
+    res.render("listings/index.ejs",{allListings});
+})
+);
 
 //new route
 app.get("/listings/new",(req, res)=>{
@@ -43,46 +47,47 @@ app.get("/listings/new",(req, res)=>{
 });
 
 //show route
-app.get("/listings/:id",async(req, res)=>{
-    let {id} =req.params;
+app.get("/listings/:id", wrapAsync(async(req, res)=>{
+    let { id } = req.params;
     const listing = await Listing.findById(id);
-    res.render("listings/show.ejs", {listing});
+    res.render("listings/show.ejs", { listing });
 })
+);
 
 //create route
-app.post("/listings", async (req, res) => {
+app.post("/listings", wrapAsync(async (req, res, next) => {
     // let { title, description, image, price, country, location } = req.body;
     const newListing = new Listing(req.body.listing);
     await newListing.save();
-    res.redirect("/listings");    
-});
+    res.redirect("/listings"); 
+
+})
+);
 
 //edit route
-app.get("/listings/:id/edit", async (req, res) => {
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
     const { id } = req.params;
     const listing = await Listing.findById(id);
-
-    if (!listing) {
-        return res.status(404).send("Listing not found");
-    }
-
-    res.render("listings/edit", { listing });
-});
+    res.render("listings/edit.ejs", { listing });
+})
+);
 
 //update route
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});
     res.redirect(`/listings/${id}`);
-});
+})
+);
 
 //delete route
-app.delete("/listings/:id",async(req, res) => {
+app.delete("/listings/:id", wrapAsync(async(req, res) => {
     let { id } = req.params;
     let deleteListing = await Listing.findByIdAndDelete(id);
     console.log(deleteListing);
     res.redirect("/listings");
 })
+);
 
 
 // app.get("/testListing", async(req,res)=>{
@@ -97,6 +102,16 @@ app.delete("/listings/:id",async(req, res) => {
 //     console.log("sample was saved");
 //     res.send("successful testing");
 // });
+app.all("*", (req, res, next) => {
+    next(new ExpressError(404, "Page not found"));
+});
+
+app.use((err, req, res, next) =>{
+    let{statusCode, message} = err;
+    res.status(statusCode).send(message);
+});
+
+
 
 app.listen(3000, () =>{
     console.log("server is listening to port 3000");
--- init/index.js
+++ init/index.js
@@ -7,14 +7,13 @@ const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 main()
 .then(()=>{
     console.log("connected to DB");
-    initDB();
 })
 .catch((err)=>{
     console.log(err);
 });
 
 async function main(){
-    await mongoose.connect(MONGO_URL)
+    await mongoose.connect(MONGO_URL);
 }
 
 const initDB = async ()=>{
